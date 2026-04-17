5f0da62bab85c7236bc0a66f4d130e337ccb2480
Tim Zimmermann <tim@linux4.de>
2026-04-17 13:16:07 +0530

    Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180

--- common.mk
+++ common.mk
@@ -9,6 +9,9 @@ COMMON_PATH := device/samsung/s5e8535-common
 # Enforce generic ramdisk allow list
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 
+# Non A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
