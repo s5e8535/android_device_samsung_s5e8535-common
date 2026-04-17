4257b1723ddacfa8f6d041622979c0474f168861
Jan Altensen <info@stricted.net>
2026-04-17 19:59:41 +0530

    s5e8535-common: exclude audiofx
    [Linux4: Move to product makefile as required after CL 299445]
    
    Change-Id: Ib38343d4d31bfd9c61cc7af1105964ddc0646335

--- common.mk
+++ common.mk
@@ -38,6 +38,8 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/audio/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+TARGET_EXCLUDES_AUDIOFX := true
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1-service \
