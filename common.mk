a72886c7190ecb045a9f42adcf6388199056e41f
Luofan Chen <clfbbn@gmail.com>
2026-04-17 22:40:01 +0530

    s5e8535-common: Build lineage health HAL
    Change-Id: I1ca5f25f3a2d8734647b9cc0ff4885b0b817c6ad

--- common.mk
+++ common.mk
@@ -86,6 +86,10 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init/init.s5e8535.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.s5e8535.rc \
     $(COMMON_PATH)/configs/init/init.s5e8535.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.s5e8535.usb.rc
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Memtrack
 PRODUCT_PACKAGES += \
     android.hardware.memtrack-service.samsung-mali
