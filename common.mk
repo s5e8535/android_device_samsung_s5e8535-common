d0e035a4868a0294c45ded9cfd3ad53e614797a8
Tim Zimmermann <tim@linux4.de>
2026-04-18 23:04:42 +0530

    s5e8535-common: Build AIDL fingerprint HAL
    Change-Id: I42b86b23cb2b086ebeecb73786b449eba32f8a79

--- common.mk
+++ common.mk
@@ -68,6 +68,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint-service.samsung
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl:64 \
