b0e58917577290a495253cddd38019ac361abe44
devhunter1 <anubhabkundu2@gmail.com>
2026-04-23 20:35:41 +0530

    s5e8535-common: Inherit dalvik-heap configs for 6GB device
    Change-Id: I71ed8f8d20ccf500a02875d55bc9f4abbd3a52d7

--- common.mk
+++ common.mk
@@ -18,6 +18,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/samsung/s5e8535-common/s5e8535-common-vendor.mk)
 
