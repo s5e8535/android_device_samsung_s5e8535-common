115396408810cd9c1c115d93ec50bf87c61698b6
Sujin Panicker <spanic@codeaurora.org>
2026-04-29 23:54:55 +0530

    s5e8535-common: Declare low-latency and pro audio capability
    Declare low-latency and pro audio capability in device
    make files to indicate low-latency and pro audio support
    exists
    
    Change-Id: I0b10d74db354889069ecadfdf4f03857a0d6506b

--- common.mk
+++ common.mk
@@ -45,6 +45,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
 TARGET_EXCLUDES_AUDIOFX := true
