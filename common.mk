7ad8a38bd216d1ec531912d97bdfd6d99fd67188
devhunter1 <anubhabkundu2@gmail.com>
2026-05-24 12:32:03 +0530

    s5e8535-common: add hardware/google/interfaces to Soong namespaces
    Change-Id: I22468db16511eb0847f5c1adda904b691e5e8f99

--- common.mk
+++ common.mk
@@ -284,6 +284,7 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH) \
+    hardware/google/interfaces \
     hardware/google/pixel \
     hardware/samsung \
     hardware/samsung_slsi/libbt \
